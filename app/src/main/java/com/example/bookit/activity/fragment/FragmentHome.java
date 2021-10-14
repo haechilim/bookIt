7ca584388df8fa92da4bee83ac468bb313f1fcc4
임준형 <haechilim@gmail.com>
2021-10-14 21:13:48 +0900

    홈화면 배너, 추천 도서 리스트 생성

--- app/src/main/java/com/example/bookit/activity/fragment/FragmentHome.java
+++ app/src/main/java/com/example/bookit/activity/fragment/FragmentHome.java
@@ -4,14 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.example.bookit.R;
 
 public class FragmentHome extends Fragment {
+    LinearLayout recommendedItems;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recommendedItems = view.findViewById(R.id.recommendedItems);
+        recommendedItems.addView(inflater.inflate(R.layout.layout_recommended_item, recommendedItems, false));
+        recommendedItems.addView(inflater.inflate(R.layout.layout_recommended_item, recommendedItems, false));
+
+        return view;
     }
 }
