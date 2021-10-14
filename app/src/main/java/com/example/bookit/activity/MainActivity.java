636e982b283b712018c8fa61bc4d7f411114f942
임준형 <haechilim@gmail.com>
2021-10-14 18:21:32 +0900

    home fragment 생성

--- app/src/main/java/com/example/bookit/activity/MainActivity.java
+++ app/src/main/java/com/example/bookit/activity/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.bookit.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.example.bookit.R;
+import com.example.bookit.activity.fragment.FragmentHome;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentTransaction transaction;
+    private FragmentHome fragmentHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        eventsBind();
+    }
+
+    private void init() {
+        fragmentHome = new FragmentHome();
+
+        transactionFragment(fragmentHome);
+    }
+
+    private void eventsBind() {
+        findViewById(R.id.homeButton).setOnClickListener(v -> transactionFragment(fragmentHome));
+    }
+
+    private void transactionFragment(Fragment fragment) {
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment).commitAllowingStateLoss();
     }
 }
