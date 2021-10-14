c702e8f20517f1d8cc0a0128d279593bb697bd8b
임준형 <haechilim@gmail.com>
2021-10-14 22:20:17 +0900

    홈화면 구현

--- app/src/main/java/com/example/bookit/activity/fragment/FragmentHome.java
+++ app/src/main/java/com/example/bookit/activity/fragment/FragmentHome.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -18,9 +19,25 @@ public class FragmentHome extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         recommendedItems = view.findViewById(R.id.recommendedItems);
-        recommendedItems.addView(inflater.inflate(R.layout.layout_recommended_item, recommendedItems, false));
-        recommendedItems.addView(inflater.inflate(R.layout.layout_recommended_item, recommendedItems, false));
+
+        addRecommendedContainer(inflater, "ㅇㅇ님을 위한 추천 도서");
+        addRecommendedContainer(inflater, "베스트 도서");
 
         return view;
+    }
+
+    private void addRecommendedContainer(LayoutInflater inflater, String title) {
+        View recommendedBook = inflater.inflate(R.layout.layout_recommended_item, recommendedItems, false);
+        LinearLayout itemsContainer = recommendedBook.findViewById(R.id.itemsContainer);
+
+        ((TextView) recommendedBook.findViewById(R.id.title)).setText(title);
+        itemsContainer.addView(inflater.inflate(R.layout.layout_book, itemsContainer, false));
+        itemsContainer.addView(inflater.inflate(R.layout.layout_book, itemsContainer, false));
+        itemsContainer.addView(inflater.inflate(R.layout.layout_book, itemsContainer, false));
+        itemsContainer.addView(inflater.inflate(R.layout.layout_book, itemsContainer, false));
+        itemsContainer.addView(inflater.inflate(R.layout.layout_book, itemsContainer, false));
+        itemsContainer.addView(inflater.inflate(R.layout.layout_book, itemsContainer, false));
+
+        recommendedItems.addView(recommendedBook);
     }
 }
